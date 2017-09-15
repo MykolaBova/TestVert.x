a5b8cde6eb98554a27248463228c591c5256e0fe
Ruslan Sheremet <ruslan.sheremet@dimetogo.com>
2017-09-15 13:04:45 +0300

    Initial commit

---
+++ src/main/java/controller/WebServer.java
@@ -0,0 +1,68 @@
+package controller;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.StaticHandler;
+import model.Whisky;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class WebServer extends AbstractVerticle {
+
+    // Store our product
+    private Map<Integer, Whisky> products = new LinkedHashMap<>();
+    // Create some product
+    private void createSomeData() {
+        Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+        products.put(bowmore.getId(), bowmore);
+        Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
+        products.put(talisker.getId(), talisker);
+    }
+
+    @Override
+    public void start(final Future<Void> startFuture) throws Exception {
+        createSomeData();
+        // Create a router object.
+        Router router = Router.router(vertx);
+
+        router.get("/api/whiskies").handler(this::getAll);
+        // Bind "/" to our hello message - so we are still compatible.
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>Hello from my first Vert.x 3 application</h1>");
+        });
+
+        // Serve static resources from the /assets directory
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
+
+        // Create the HTTP server and pass the "accept" method to the request handler.
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(
+                        // Retrieve the port from the configuration,
+                        // default to 8080.
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                startFuture.complete();
+                            } else {
+                                startFuture.fail(result.cause());
+                            }
+                        }
+                );
+    }
+
+    private void getAll(RoutingContext routingContext) {
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(products.values()));
+    }
+}
