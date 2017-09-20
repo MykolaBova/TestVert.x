37faacb000d0bf40b32a4f9c57c6d42e168f1bc0
Ruslan Sheremet <ruslan.sheremet@dimetogo.com>
2017-09-20 09:54:50 +0300

    disable senders added

--- src/main/java/com/adsology/Main.java
+++ src/main/java/com/adsology/Main.java
@@ -7,7 +7,11 @@ import io.vertx.ext.web.RoutingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -38,30 +42,41 @@ public class Main {
 //                .forEach();
 
         // Create consumers
+        List<String> addresses = IntStream.range(0, receivers)
+                .mapToObj(String::valueOf)
+                .collect(toList());
         for (int i = 0; i < receivers; i++) {
-            vertx.deployVerticle(new ConsumeVerticle(String.valueOf(i)));
+            vertx.deployVerticle(new ConsumeVerticle(addresses.get(i)));
         }
 
         // Create senders
 
         int sendDelay = messagesPerSecond * 1000 * senders;
-        int loop = messages / senders;
         if (senders > receivers) {
-            int receiver = 0;
+            int loop = messages / senders;
+            int index = 0;
             for (int i = 0; i < senders; i++) {
-                vertx.deployVerticle(new SenderVerticle(sendDelay, loop, String.valueOf(receiver)));
-                receiver = receiver < receiver - 1 ? ++receiver : 0 ;
+                vertx.deployVerticle(new SenderVerticle(
+                        sendDelay,
+                        loop,
+                        Collections.singletonList(
+                                addresses.get(index))));
+                index = index < index - 1 ? ++index : 0 ;
             }
         } else if (senders < receivers) {
-//            int receiversPerSender = receivers / senders;
-//            int lastReceiver = 0;
-//            for (int i = 0; i < senders; i++) {
-//                String[] addresses =;
-//                for (int j = lastReceiver; j < lastReceiver + receiversPerSender; j++) {
-//                    addresses[]
-//                }
-//                vertx.deployVerticle(new SenderVerticle(sendDelay, loop, ));
-//            }
+            int loop = messages / receivers;
+
+            int receiversPerSender = receivers / senders;
+            int lastIndex = 0;
+            for (int i = 0; i < senders; i++) {
+                vertx.deployVerticle(new SenderVerticle(
+                        sendDelay,
+                        loop,
+                        addresses.subList(
+                                lastIndex,
+                                lastIndex + receiversPerSender)));
+                lastIndex += receiversPerSender;
+            }
         }
 
         routingContext.response()
--- src/main/java/com/adsology/SenderVerticle.java
+++ src/main/java/com/adsology/SenderVerticle.java
@@ -7,14 +7,16 @@ import io.vertx.core.eventbus.EventBus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+
 public class SenderVerticle extends AbstractVerticle {
     private static Logger logger = LogManager.getLogger(SenderVerticle.class);
 
-    private final String[] addresses;
+    private final List<String> addresses;
     private final long delay;
     private final int loop;
 
-    public SenderVerticle(int delay, int loop, String... addresses) {
+    public SenderVerticle(int delay, int loop, List<String> addresses) {
         this.addresses = addresses;
         this.delay = delay;
         this.loop = loop;
@@ -22,16 +24,14 @@ public class SenderVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) throws Exception {
-        EventBus eb = vertx.eventBus();
-
         vertx.setPeriodic(delay, new Handler<Long>() {
             int count = 0;
 
             @Override
             public void handle(Long event) {
-                for (int i = 0; i < addresses.length; i++) {
-                    vertx.eventBus().send(addresses[i], "ping");
-                    logger.info("Sender send message to {}", addresses[i]);
+                for (int i = 0; i < addresses.size(); i++) {
+                    vertx.eventBus().send(addresses.get(i), "ping");
+                    logger.info("Sender send message to {}", addresses.get(i));
                 }
 
                 if (++count > loop) {
